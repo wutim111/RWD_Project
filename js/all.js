fe44cde097ba70f114baa91f5af6ced70f29fbb8
wutim111 <wutim111@gmail.com>
2023-09-23 11:08:18 +0800

    連結選單

--- js/all.js
+++ js/all.js
@@ -1,6 +1,30 @@
 $(document).ready(function () {
     $('.menu_btn').on('click', function (e) {
-        e.preventDefault()
+        e.preventDefault();
         $('body').toggleClass('menu_open');
+        $('.link_menu').slideUp();
+    });
+    $('.menu_link').on('click', function (e) {
+        e.preventDefault();
+        $('.link_menu').slideToggle();;
+    });
+    
+    $('.menu_feature').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop : $('.feature').offset().top -50
+        },1000);
+    });
+    $('.menu_chef').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop : $('.chef').offset().top -50
+        },1000);
+    });
+    $('.menu_map').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop : $('.booking').offset().top -50
+        },1000);
     });
 });
